361c52a810c1cb15c850fc80bc7864551cc66cca
kazu <kazu8160@gmail.com>
2019-05-11 16:02:21 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
 type Callback<T> = () => Promise<T>
 type Options = { retry?: number }
 
-export function ritry<T>(callback: Callback<T>, options: Options = {}): Promise<T> {
+export async function ritry<T>(callback: Callback<T>, options: Options = {}): Promise<T> {
   const mergedOpts = { retry: 1, ...options }
 
   if (mergedOpts.retry < 1) {
     throw new Error('The number of retries must be at least one')
   }
 
-  return (async (): Promise<T> => {
-    let result: T = undefined as any
-    let count = 1
+  let result: T = undefined as any
+  let count = 1
 
-    for (; count <= mergedOpts.retry; count++) {
-      try {
-        result = await callback()
-        break
-      } catch (err) {
-        if (count >= mergedOpts.retry) {
-          throw err
-        }
+  for (; count <= mergedOpts.retry; count++) {
+    try {
+      result = await callback()
+      break
+    } catch (err) {
+      if (count >= mergedOpts.retry) {
+        throw err
       }
     }
+  }
 
-    return result
-  })()
+  return result
 }
