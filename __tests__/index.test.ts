fbf9878c225ecf84cbf529015d3dfaf9840c8a94
kazu <kazu8160@gmail.com>
2019-05-14 21:44:43 +0900

    get retry count

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -33,3 +33,27 @@ test('should be run once by default', async () => {
   await expect(ritry(mock)).rejects.toThrow('fail')
   expect(mock).toBeCalledTimes(2)
 })
+
+test('should be able to get the number of retries', async () => {
+  const numbers1: number[] = []
+  const numbers2: number[] = []
+
+  try {
+    await ritry(({ retryCount }) => {
+      numbers1.push(retryCount)
+      return Promise.reject(new Error('fail'))
+    }, { retry: 4 })
+  } catch(_) {
+    expect(numbers1).toEqual([0, 1, 2, 3, 4])
+  }
+
+  await ritry(({ retryCount }) => {
+    return new Promise((resolve, reject) => {
+      numbers2.push(retryCount)
+      if (retryCount < 3) return reject(new Error('fail'))
+      resolve()
+    })
+  }, { retry: 4 })
+
+  expect(numbers2).toEqual([0, 1, 2, 3])
+})
